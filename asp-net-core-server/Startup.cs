046905c7f81bfdd3d2d8977f6d8bf8b05379ddde
zakhodyaeva <margarita.zakhodyaeva@devexpress.com>
2022-06-28 11:23:17 +0400

    update project

--- asp-net-core-server/Startup.cs
+++ asp-net-core-server/Startup.cs
@@ -2,18 +2,21 @@ using DevExpress.AspNetCore;
 using DevExpress.DashboardAspNetCore;
 using DevExpress.DashboardCommon;
 using DevExpress.DashboardWeb;
+using DevExpress.DataAccess.Json;
 using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.DataProtection;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.FileProviders;
+using System;
 
-namespace AspNetCoreDashboardBackend {
-    public class Startup {
-        // This method gets called by the runtime. Use this method to add services to the container.
-        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
-
-        public Startup(IConfiguration configuration, IWebHostEnvironment hostingEnvironment) {
+namespace AspNetCoreDashboardBackend
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration, IWebHostEnvironment hostingEnvironment)
+        {
             Configuration = configuration;
             FileProvider = hostingEnvironment.ContentRootFileProvider;
         }
@@ -21,10 +24,10 @@ namespace AspNetCoreDashboardBackend {
         public IConfiguration Configuration { get; }
         public IFileProvider FileProvider { get; }
 
-        // This method gets called by the runtime. Use this method to add services to the container.
-        public void ConfigureServices(IServiceCollection services) {
-            // Configures services to use the Web Dashboard Control.
+        public void ConfigureServices(IServiceCollection services)
+        {
             services
+                // Configures CORS policies.                
                 .AddCors(options => {
                     options.AddPolicy("CorsPolicy", builder => {
                         builder.AllowAnyOrigin();
@@ -32,42 +35,52 @@ namespace AspNetCoreDashboardBackend {
                         builder.WithHeaders("Content-Type");
                     });
                 })
+                // Adds the DevExpress middleware.
                 .AddDevExpressControls()
-                .AddControllers()
-                .AddDefaultDashboardController(configurator => {
-                    configurator.SetDashboardStorage(new DashboardFileStorage(FileProvider.GetFileInfo("App_Data/Dashboards").PhysicalPath));
-                    configurator.SetDataSourceStorage(CreateDataSourceStorage());
-                });
-        }
-
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env) {
-            var supportedCultures = new[] { "en-US", "de-DE" };
-            var opts = new RequestLocalizationOptions()
-                .SetDefaultCulture(supportedCultures[1])
-                .AddSupportedCultures(supportedCultures)
-                .AddSupportedUICultures(supportedCultures);
-            opts.RequestCultureProviders.Clear();
-            app.UseRequestLocalization(opts);
-
-            // Registers the DevExpress middleware.
-            app.UseDevExpressControls();
-            app.UseRouting();
-            app.UseCors("CorsPolicy");
-            app.UseEndpoints(endpoints => {
-                // Maps the dashboard route.
-                EndpointRouteBuilderExtension.MapDashboardRoute(endpoints, "api/dashboard");
-                // Requires CORS policies.
-                endpoints.MapControllers().RequireCors("CorsPolicy");
+                // Adds controllers.
+                .AddControllers();
+            // Configures the dashboard backend.
+            services.AddScoped<DashboardConfigurator>((IServiceProvider serviceProvider) => {
+                DashboardConfigurator configurator = new DashboardConfigurator();
+                configurator.SetDashboardStorage(new DashboardFileStorage(FileProvider.GetFileInfo("App_Data/Dashboards").PhysicalPath));
+                configurator.SetDataSourceStorage(CreateDataSourceStorage());
+                return configurator;
             });
+
+
         }
 
-        public DataSourceInMemoryStorage CreateDataSourceStorage() {
+        public DataSourceInMemoryStorage CreateDataSourceStorage()
+        {
             DataSourceInMemoryStorage dataSourceStorage = new DataSourceInMemoryStorage();
             DashboardObjectDataSource objDataSource = new DashboardObjectDataSource("ObjectDataSource", typeof(ProductSales));
             objDataSource.DataMember = "GetProductSales";
             dataSourceStorage.RegisterDataSource("objectDataSource", objDataSource.SaveToXml());
             return dataSourceStorage;
+        }
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            // Registers the DevExpress middleware.            
+            app.UseDevExpressControls();
+            // Registers routing.
+            app.UseRouting();
+            // Registers CORS policies.
+            app.UseCors("CorsPolicy");
+            app.UseEndpoints(endpoints => {
+                // Maps the dashboard route.
+                EndpointRouteBuilderExtension.MapDashboardRoute(endpoints, "api/dashboard", "DefaultDashboard");
+                // Requires CORS policies.
+                endpoints.MapControllers().RequireCors("CorsPolicy");
+            });
+        }
+
+    }
+    public class DefaultDashboardController : DashboardController
+    {
+        public DefaultDashboardController(DashboardConfigurator configurator, IDataProtectionProvider dataProtectionProvider = null)
+         : base(configurator, dataProtectionProvider)
+        {
         }
     }
 }
